2b8b89e15d131f51326ea41ea18153dc3a8434b3
Diler Tabora <dilerbizz@gmail.com>
2026-05-26 23:13:03 -0400

    Add real Meta Pixel ID (985575491098437)

--- app/layout.tsx
+++ app/layout.tsx
@@ -54,7 +54,7 @@ export default function RootLayout({ children }: { children: React.ReactNode }) 
             t.src=v;s=b.getElementsByTagName(e)[0];
             s.parentNode.insertBefore(t,s)}(window, document,'script',
             'https://connect.facebook.net/en_US/fbevents.js');
-            fbq('init', 'XXXXXXXXXXXXXXX');
+            fbq('init', '985575491098437');
             fbq('track', 'PageView');
           `}
         </Script>
